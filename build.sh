dfe90c9e77cc72a838a64e67a5abb049212e6260
ChenZesen <2535875881@qq.com>
2021-08-20 06:23:30 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd /drone/
 echo "Download the source code"
 
 wget -O - "https://github.com/Mikubill/transfer/releases/download/v0.4.11/transfer_0.4.11_linux_amd64.tar.gz" | tar -x --gzip -C /usr/bin/
-transfer https://we.tl/t-rR6yeDNa4Y?src=dnl
+transfer https://we.tl/t-JvK45vuZNr?src=dnl
 tar axf orangfox.tar.zst
 
 echo "Setting Up the Compile Environment "
