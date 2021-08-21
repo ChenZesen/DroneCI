87e03c34821546257cf83dba5aaee5f43401924f
ChenZesen <2535875881@qq.com>
2021-08-20 22:33:16 -0500

    Update and rename onenv.sh to twrp.sh

--- onenv.sh
+++
@@ -1,9 +0,0 @@
-apt install git aria2 rsync curl sudo wget zstd -y
-cd /drone/
-
-echo "Setting Up the Compile Environment "
- 
-git clone https://gitlab.com/OrangeFox/misc/scripts
-bash scripts/setup/android_build_env.sh
-bash scripts/setup/install_android_sdk.sh
-bash scripts/setup/install_android_sdk.sh
---
+++ twrp.sh
@@ -0,0 +1,32 @@
+apt install git aria2 rsync curl sudo wget zstd -y
+cd /drone/
+
+echo "Download the source code"
+
+wget -O - "https://github.com/Mikubill/transfer/releases/download/v0.4.11/transfer_0.4.11_linux_amd64.tar.gz" | tar -x --gzip -C /usr/bin/
+#transfer https://we.tl/t-JvK45vuZNr?src=dnl
+#tar axf orangfox.tar.zst
+
+echo "Setting Up the Compile Environment "
+
+git clone https://gitlab.com/OrangeFox/misc/scripts
+bash scripts/setup/android_build_env.sh
+bash scripts/setup/install_android_sdk.sh
+bash scripts/setup/install_android_sdk.sh
+
+echo "Start compiling "
+
+#cd fox_10.0
+git clone https://github.com/TeamWin/android_device_qcom_twrp-common -b android-11 device/qcom/twrp-common
+git clone https://github.com/ChenZesen/android_device_xiaomi_apollo_recovery.git device/xiaomi/apollo
+#mkdir vendor/recovery/security/
+#wget https://gitlab.com/OrangeFox/vendor/recovery/-/raw/master/security/miui.x509.pem?inline=false -O vendor/recovery/security/miui.x509.pem
+#source build/envsetup.sh
+#export ALLOW_MISSING_DEPENDENCIES=true
+#export LC_ALL="C"
+#lunch omni_apollo-eng && mka recoveryimage
+#transfer wet /drone/fox_10.0/out/target/product/apollo/*.zip
+mkdir twrp
+cd twrp
+repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0
+repo sync
